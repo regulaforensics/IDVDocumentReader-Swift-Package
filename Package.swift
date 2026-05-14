b3626d6a1ab3424f86ee933e5f71fa764d381339
Jenkins <jenkins@regula.by>
2026-05-14 10:54:16 +0300

    Release 3.6.3109

--- Package.swift
+++ Package.swift
@@ -18,19 +18,19 @@ let package = Package(
         .package(
             name: "IDVModule",
             url: "https://github.com/regulaforensics/IDVModule-Swift-Package.git",
-            from: "3.1.1203"
+            from: "3.6.1740"
         ),
         .package(
             name: "DocumentReader",
             url: "https://github.com/regulaforensics/DocumentReader-Swift-Package.git",
-            from: "8.2.5052"
+            from: "9.3.6250"
         ),
     ],
     targets: [
         .binaryTarget(
             name: packageName,
-            url: "https://pods.regulaforensics.com/\(packageName)/3.1.1826/\(packageName)-3.1.1826.zip",
-            checksum: "686886d08fdb85e4703badaee7cf491cb7d25a7cce64a77323590e1a8208bf23"
+            url: "https://pods.regulaforensics.com/\(packageName)/3.6.3109/\(packageName)-3.6.3109.zip",
+            checksum: "4c1d272081bf0beb8825ae7d696a07a8ca50a0cf850edd6af6a749e9f6d66e35"
         ),
         .target(
             name: "\(packageName)Common",
